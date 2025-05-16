c155fea4ad6316ef211896f9904c70bb5a765279
matheus <matheushenrrique3298@gmail.com>
2025-05-16 20:46:51 -0300

    Add files via upload

---
+++ projeto git.py 2.py
@@ -0,0 +1,18 @@
+print 
+numero1 = int(input("Escreva quantos dias foram trabalhados: "))
+numero2= 8
+valor = 6.00
+resultado = numero1 * numero2
+receber = resultado * valor
+print("INFORMAÇOES GERAIS")
+print(".....................................")
+print ("horas trabalhadas:", resultado)
+
+print(".....................................")
+
+print("dias trabalhados:", numero1)
+
+print(".....................................")
+
+print("valor a receber::", receber)
+
